e0f6ac59b8da7ac150c5e8b84ca100775463d605
Shivam Bhatt <shivambhatt97@gmail.com>
2022-11-03 16:27:09 +0530

    add a controller and router for home

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ const homeController = require('../controllers/home_controller');
 
 console.log('router loaded ');
 router.get('/', homeController.home);
+router.use('/users',require('./users'));
+router.use('/',require('./post'));
+// for any further routes, access from here
+// router.get('/test',homeController.actionName);
 
-router.get('/test',homeController.actionName);
 
 
 module.exports = router;
