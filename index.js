6afa515a4734d307dc0d406f9f64c95f731d6b4f
Shivam bhatt <shivambhatt97@gmail.com>
2022-11-15 23:48:47 +0530

    setup

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express= require('express');
+const bodyParser=require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+
 const expressLayouts = require('express-ejs-layouts');
+
+const db=require('./config/mongoose');
+const user=require('./models/user');
+
+app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true}));
 //extract style and script from subpages into the layout
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
+
 
 //use express router
 app.use('/',require('./routes'));
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ console.log('router loaded ');
 router.get('/', homeController.home);
 router.use('/users',require('./users'));
 router.use('/',require('./post'));
+
 // for any further routes, access from here
 // router.get('/test',homeController.actionName);
 
